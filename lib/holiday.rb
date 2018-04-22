1f6499c807eb651cd516a6bddce23ad4c042ae51
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-22 19:39:50 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,7 +79,6 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect do |season, holidays_hash|
      holidays_hash.collect do |holiday, supply_array|
        supply_array.each {|supply| supply == "BBQ"}
-      end
     end
   end
 end
