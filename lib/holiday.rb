94122e0b5c482e5d178fa8c980d343e60128926c
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-22 18:18:58 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.collect do |season, holidays|
-    puts "#{season.to_s.capitalize}: #{holidays.to_s}:"
+    puts "#{season.to_s.capitalize}: #{holidays.to_s.capitalize}:"
   end
 end
 
