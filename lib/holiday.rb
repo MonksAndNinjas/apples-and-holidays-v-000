7bfe9cd74cc941347f88f776f781500c689d4cdf
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-22 18:53:57 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
 
        holiday_array = holiday.to_s.split("_")
 
-   puts " #{(holiday_array.collect do |word|
+   puts "   #{(holiday_array.collect do |word|
          word.capitalize
        end).join(" ")}: #{supply_array.join(", ")}"
      end
