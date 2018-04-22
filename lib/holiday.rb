8acbc532808adb0bffcfdb400d698db40478d9b3
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-22 19:32:36 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,10 +77,9 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
   holiday_hash.each do |season, holidays_hash|
-     holidays_hash.each do |holiday, supply_array|
+     holidays_hash.collect do |holiday, supply_array|
       if supply_array.any? {|supply| supply == "BBQ"} == true
         holiday
-        binding.pry
       end
     end
   end
